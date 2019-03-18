3e88e4a850b28483f142221292b854c7b750c96d
Sameer Kumar <sameer18051998@gmail.com>
2019-03-19 00:47:40 +0530

    Cors Support Added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const buzzwords = require('./buzzwords')
-const app = express()
+const express = require('express');
+const buzzwords = require('./buzzwords');
+const cors = require('cors');
+const app = express();
+app.use(cors({credentials: true, origin: true}));
+
 app.get('/', function(request, response) {
   response.send({'phrase':buzzwords.buzzwords()})
-})
-app.listen(process.env.PORT || 3000, () => console.log('Listening...'))
+});
+app.listen(process.env.PORT || 3000, () => console.log('Listening...'));
